0806ee6ffd5b7d7722addbcbffa54d636836c22d
Dofault <98189547+Dofault@users.noreply.github.com>
2025-03-11 21:50:51 +0100

    change

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
+  <link href="https://fonts.googleapis.com/css2?family=Jersey+25&display=swap" rel="stylesheet">
+
   <div class="bg-gradient-to-r from-slate-200 via-slate-400 from-slate-200 h-[3000px]">
 
+    <navbar></navbar>
     <div class="pt-0">
       <NuxtPage />
     </div>
@@ -8,4 +11,7 @@
 </template>
 
 <style>
+  body {
+    font-family: "Jersey 25", sans-serif;
+  }
 </style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,10 @@
 <template>
     
-    <div class="hidden md:block">
-      <div class="top-0 left-0 w-full flex justify-center items-center bg-neutral-700 z-50">
-    <div class="flex backdrop-blur-md bg-white rounded-b-xl bg-opacity-30 border-b h-16 border-gray-200 dark:border-gray-700">
+    <div class="hidden md:block fixed top-0 left-0 w-full z-50 flex justify-center items-center">
+      <div class="top-0 left-0 w-full flex justify-center items-centerz-50">
+    <div class="flex backdrop-blur-md bg-[#1F2833] bg-opacity-90 rounded-b-xl border-b h-16 border-gray-200 dark:border-gray-700">
 
-
-    <NuxtLink to="/vote" class="flex items-center h-15 px-2 py-2 -mb-px text-center text-black bg-transparent border-b-2 border-transparent md:px-5 -px-1 dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400">
+    <NuxtLink to="/vote" class="flex items-center h-15 px-2 py-2 -mb-px text-center text-white bg-transparent border-b-2 border-transparent md:px-5 -px-1 dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400">
 
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mx-1 sm:w-8 sm:h-8">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
@@ -14,7 +13,7 @@
     </NuxtLink>
 
 
-    <NuxtLink to="/boutique" class="flex items-center h-15 px-2 py-2 -mb-px text-center text-black bg-transparent border-b-2 border-transparent md:px-5 -px-1 dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400">
+    <NuxtLink to="/boutique" class="flex items-center h-15 px-2 py-2 -mb-px text-center text-white bg-transparent border-b-2 border-transparent md:px-5 -px-1 dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400">
 
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mx-1 sm:w-8 sm:h-8">
         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
@@ -37,7 +36,7 @@
 
 
     
-      <button class="flex items-center h-15 px-2 py-2 -mb-px text-center text-black bg-transparent border-b-2 border-transparent md:px-5 -px-1 dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400">
+      <button class="flex items-center h-15 px-2 py-2 -mb-px text-center text-white bg-transparent border-b-2 border-transparent md:px-5 -px-1 dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400">
 
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mx-1 sm:w-8 sm:h-8">
           <path stroke-linecap="round" stroke-linejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
@@ -47,7 +46,7 @@
         </button>
 
 
-      <LoginLink v-if="!$auth.loggedIn" to="/api/login" class="flex items-center h-15 px-2 py-2 -mb-px text-center text-black bg-transparent border-b-2 border-transparent md:px-5 -px-1 dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400">
+      <LoginLink v-if="!$auth.loggedIn" to="/api/login" class="flex items-center h-15 px-2 py-2 -mb-px text-center text-white bg-transparent border-b-2 border-transparent md:px-5 -px-1 dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400">
 
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mx-1 sm:w-8 sm:h-8">
           <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -67,17 +66,15 @@
       </div>
     </div>
 
-<div class="fixed top-4 right-4">
+<div class="fixed top-4 z-50 right-4">
       <div class=" justify-end md:hidden">
-        <button type="button" @click="open = !open" class="relative inline-flex items-center justify-center rounded-md bg-color1 p-2 text-color4 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-color4" aria-controls="mobile-menu" aria-expanded="false">
+        <button type="button" :class="{ 'hidden': open } "@click="open = !open" class="relative inline-flex items-center justify-center rounded-md bg-color1 p-2 text-color4 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-color4" aria-controls="mobile-menu" aria-expanded="false">
           <span class="absolute -inset-0.5"></span>
           <span class="sr-only">Open main menu</span>
           <svg :class="{ 'block': !open, 'hidden': open }" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
-          <svg :class="{ 'block': open, 'hidden': !open }" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
+
         </button>
       </div>
     </div>
